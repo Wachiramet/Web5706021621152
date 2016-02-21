6678d85c0295286cbbd290b1404a0dc66eb03631
Wachiramet <wachiramet.pai@gmail.com>
2016-02-21 21:48:01 +0700

    Random

--- random.php
+++ random.php
@@ -7,14 +7,23 @@
   <body>
     <center>
       <h1>RANDOM</h1>
-        <?php
-          for($i=0;$i<52;$i++){
-            $num[$i] = rand(1,52);
+      <form action="#" method="post">
+        Rows : <input type="number" name="row"><br>
+        Input : <input type="number" name="colum"><br>
+        <input type="submit" value="Submit"><br><br>
+      </form>
+      <?php
+        if($_POST){
+          $row = $_POST["row"];
+          $col = $_POST["colum"];
+
+          for($i=0;$i<$row*$col;$i++){
+            $num[$i] = rand(1,$row*$col);
             $n=0;
             while($n < $i){
               if($num[$i] == $num[$n]){
                 //echo "New random $i ".$num[$i];
-                $num[$i] = rand(1,52);
+                $num[$i] = rand(1,$row*$col);
                 $n = 0;
                 //echo " > ".$num[$i]."<br>";
               }
@@ -27,13 +36,14 @@
             echo "<tr>";
             for($c=1;$c<=count($num);$c++){
               echo "<td>".$num[$c-1]."</td>";
-              if($c % 4 == 0){
+              if($c % $col == 0){
                 echo "</tr><tr>";
               }
             }
             echo "</tr>";
           echo "</table>";
-        ?>
-      </center>
+        }
+      ?>
+    </center>
   </body>
 </html>
